d9dc54250e1d3964d63ebe7a02cb5769c16e8ab8
Suren_Hakobyan <surhak99@gmail.com>
2024-11-23 23:03:29 +0400

    Created an initial constants file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import constants
 
 """
     lets say 16 bit 0000000000000000
@@ -8,16 +9,16 @@ import matplotlib.pyplot as plt
     Movement directions: only horizontal and vertical
     
     Bit management: Starting from the left
-    1. Speed - The amount of cells that the boz can move in a single time step.
+    1. Speed - The amount of cells that the creature can move in a single time step.
                2 bits (from 1 to 4 units per time step)
     
-    2. Eyesight - The amount of cells that the boz sees at any time.
+    2. Eyesight - The amount of cells that the creature sees at any time.
                   3 bits (from 0 to 7 units)
     
-    3. Aggression - The probability that the boz will engage in a battle if possible. Two bozes can engage in a battle if they are in adjacent cells and one of them decided to do so.
+    3. Aggression - The probability that the creature will engage in a battle if possible. Two creatures can engage in a battle if they are in adjacent cells and one of them decided to do so.
                     3 bits (from 0 to 7 units, percent = (value * 100)/(2^(number of bits) - 1))
     
-    4. Strength - The physical power unit each boz has.
+    4. Strength - The physical power unit each creature has.
                   4 bits (from 1 to 16 units)
                   To be discussed: the aggressor has an advantage or no? (+1 strength ??)
                   
