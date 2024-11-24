fdda8343da311f7513070ad6b9049548740dd21c
Mher Saribekyan <saribekyanmher@gmial.com>
2024-11-24 22:51:32 +0400

    Added basic creature functions

--- creature_service.py
+++ creature_service.py
@@ -1,9 +1,19 @@
-import constants
+import numpy as np
+from random import random
+from constants import BASE_ENERGY_LEVEL, PROBABILITY_OF_INDIVIDUAL_GENE_MUTATION, ENERGY_GAINED_FROM_EATING_FOOD
 
 def create_creature(gene):
-    pass
+    creature = [gene, BASE_ENERGY_LEVEL]
+    return creature
 
 def get_max_energy(gene):
+    max_energy = BASE_ENERGY_LEVEL * get_stamina()
+    return max_energy
+
+def eat_food(creature):
+    creature[2] += ENERGY_GAINED_FROM_EATING_FOOD
+
+def eat_creature(creature, other_creature):
     pass
 
 def get_speed(gene):
@@ -21,11 +31,29 @@ def get_strength(gene):
 def get_stamina(gene):
     pass
 
-def eat_food(creature):
+def mutate_speed(gene):
     pass
 
-def eat_creature(creature, other_creature):
+def mutate_eyesight(gene):
+    pass
+
+def mutate_aggression(gene):
+    pass
+
+def mutate_strength(gene):
+    pass
+
+def mutate_stamina(gene):
     pass
 
 def reproduce(creature):
-    pass
+    new_gene = creature[0]
+
+    new_gene = mutate_speed(new_gene)
+    new_gene = mutate_eyesight(new_gene)
+    new_gene = mutate_aggression(new_gene)
+    new_gene = mutate_strength(new_gene)
+    new_gene = mutate_stamina(new_gene)
+
+    new_creature = [new_gene, BASE_ENERGY_LEVEL]
+    return new_creature
