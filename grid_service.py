00407904c7c51be8f76c80926d589f666cc80928
Davit Badalyan <dbadalyan2006@gmail.com>
2024-11-27 12:22:04 +0400

    Added Random Walk and Food Walk implementation

--- grid_service.py
+++ grid_service.py
@@ -1,5 +1,6 @@
 from creature_service import creatures
 from food_service import foods
+from numpy import random
 
 def get_position(creature):
     x = (creature & (0b111111 << 10)) >> 10
@@ -29,8 +30,45 @@ def get_foods_in_eyesight(eyesight):
 def move(creature):
     pass
 
-def move_towards_food(creature, food):
-    pass
+def move_towards_food(creature, food) -> bool:
+    creature_x, creature_y = get_position(creature)
+    food_x = (food & (0b111111 << 6)) >> 6
+    food_y = food & 0b111111
+
+    if food_x > creature_x and creature_x < 63:
+        creature_x += 1
+    elif food_x < creature_x and creature_x > 0:
+        creature_x -= 1
+
+    if food_y > creature_y and creature_y < 63:
+        creature_y += 1
+    elif food_y < creature_y and creature_y > 0:
+        creature_y -= 1
+
+    creature = (creature & ~(0b111111 << 10)) | (creature_x << 10)
+    creature = (creature & ~(0b111111 << 4)) | (creature_y << 4)
+
+    return True
 
 def get_viewable_cells(eyesight):
     pass
+
+def move_random(creature) -> bool:
+    # when you can't move to a good food source.
+
+    x, y = get_position(creature)
+    direction = random.choice(['up', 'down', 'left', 'right'])
+
+    if direction == 'up' and y < 63:
+        y += 1
+    elif direction == 'down' and y > 0:
+        y -= 1
+    elif direction == 'left' and x > 0:
+        x -= 1
+    elif direction == 'right' and x < 63:
+        x += 1
+
+    creature = (creature & ~(0b111111 << 10)) | (x << 10)
+    creature = (creature & ~(0b111111 << 4)) | (y << 4)
+
+    return True
